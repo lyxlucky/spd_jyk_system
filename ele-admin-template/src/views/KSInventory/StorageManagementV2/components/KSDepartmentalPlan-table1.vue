927c02728a2006c0ef5e6388ff90165fdaf3ed8e
zhb <zouhaibing1@gmail.com>
2024-06-11 17:49:37 +0800

    z1

---
+++ ele-admin-template/src/views/KSInventory/StorageManagementV2/components/KSDepartmentalPlan-table1.vue
@@ -0,0 +1,334 @@
+
+<template>
+  <div class="ele-body">
+    <el-descriptions class="margin-top" :column="3" border>
+      <!-- <template slot="extra">
+        <el-button type="primary" size="small">操作</el-button>
+      </template> -->
+      <el-descriptions-item labelStyle="background: #D7E6FA;">
+        <template slot="label">
+          供应商名称
+        </template>
+        深圳市华西卫材有限公司
+      </el-descriptions-item>
+      <el-descriptions-item labelStyle="background: #D7E6FA;">
+        <template slot="label">
+          计划单号
+        </template>
+        JH182452134521
+      </el-descriptions-item>
+      <el-descriptions-item labelStyle="background: #D7E6FA;">
+        <template slot="label">
+          计划日期
+        </template>
+        2023-05-15
+      </el-descriptions-item>
+      <el-descriptions-item labelStyle="background: #D7E6FA;">
+        <template slot="label">
+          备注
+        </template>
+
+      </el-descriptions-item>
+
+    </el-descriptions>
+
+  </div>
+
+</template>
+
+<script>
+import KSDepartmentalPlanSearch from './KSDepartmentalPlan-search.vue';
+import {
+  SerachPlanList,
+  DeletePlanList,
+  SearchHistoryConsumedAndPurchaseDept,
+  ReturnInitState
+} from '@/api/KSInventory/KSDepartmentalPlan';
+import { getDeptAuthVarNew } from '@/api/KSInventory/KSInventoryBasicData';
+export default {
+  name: 'KSDepartmentalPlanTable',
+  props: ['IsReload'],
+  components: {
+    KSDepartmentalPlanSearch
+  },
+  data() {
+    return {
+      SerachName: '',
+      SerachName2: '',
+
+      // 表格列配置
+      columns: [
+        // {
+        //   columnKey: 'selection',
+        //   type: 'selection',
+        //   width: 45,
+        //   align: 'center',
+        //   fixed: 'left'
+        // },
+        {
+          columnKey: 'index',
+          type: 'index',
+          width: 45,
+          align: 'center',
+          showOverflowTooltip: true,
+          fixed: 'left'
+        },
+        {
+          columnKey: 'action',
+          label: '操作',
+          width: 120,
+          align: 'center',
+          resizable: false,
+          slot: 'action',
+          showOverflowTooltip: true,
+          fixed: 'right'
+        },
+        {
+          prop: 'PlanNum',
+          label: '申领单号',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 110
+        },
+        {
+          prop: 'Operater',
+          label: '申领人',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 80
+        },
+        {
+          // prop: 'State',
+          label: '状态',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 110,
+          slot: 'State'
+          // formatter: (row, column, cellValue) => {
+          //   if (cellValue === '0') {
+          //     return '新增';
+          //   } else if (cellValue === '1') {
+          //     return '已提交';
+          //   } else if (cellValue === '2') {
+          //     return '配送中';
+          //   } else if (cellValue === '5') {
+          //     return '已审核';
+          //   } else if (cellValue === '10') {
+          //     return '强制结束';
+          //   } else if (cellValue === '6' || cellValue === '4') {
+          //     return '已审批';
+          //   } else {
+          //     return '配送中';
+          //   }
+          // }
+        },
+        {
+          prop: 'DEPT_TWO_NAME',
+          label: '科室名称',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 110
+        },
+        {
+          prop: 'PlanTime',
+          label: '申领时间',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 110
+        },
+        {
+          prop: 'Approval_Time',
+          label: '审批时间',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 180,
+          formatter: (row, column, cellValue) => {
+            return this.$util.toDateString(cellValue);
+          }
+        },
+        {
+          prop: 'BZ',
+          label: '备注',
+
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 110
+        }
+      ],
+      toolbar: false,
+      pageSize: 2,
+      pagerCount: 2,
+      pageSizes: [10, 20, 50, 100, 9999999],
+      // 表格选中数据
+      selection: [],
+      // 当前编辑数据
+      current: null,
+      // 是否显示编辑弹窗
+      showEdit: false,
+      // 是否显示导入弹窗
+      showImport: false,
+      // datasource: [],
+      data: [],
+      applyPlanSbz: 0,
+      applyPlanXhz: 0,
+      applyPlanBl: '0%',
+      key: 0,
+      RenderTabel: true
+    };
+  },
+  mounted() {
+    this.$bus.$on('handleCommand', (data) => {
+      // this.RenderTabel = !this.RenderTabel;
+
+      this.reload();
+      this.RenderTabel = false;
+      // 建议加上 nextTick 微任务
+      // 否则在同一事件内同时将tableShow设置false和true有可能导致组件渲染失败
+      this.$nextTick(function () {
+        // 加载
+        this.RenderTabel = true;
+      });
+    });
+  },
+  methods: {
+    /* 表格数据源 */
+    datasource({ page, limit, where, order }) {
+      var Dept_Two_CodeStr = '';
+      var userDeptList = this.$store.state.user.info.userDept;
+      for (let i = 0; i < userDeptList.length; i++) {
+        Dept_Two_CodeStr =
+          Dept_Two_CodeStr + userDeptList[i].Dept_Two_Code + ',';
+      }
+      where.DeptCode = Dept_Two_CodeStr;
+      let data = SerachPlanList({ page, limit, where, order }).then((res) => {
+        var tData = {
+          count: res.total,
+          list: res.result
+        };
+        return tData;
+      });
+      return data;
+    },
+    /* 刷新表格 */
+    reload(where) {
+      this.$refs.table.reload({ page: 1, where: where });
+    },
+    onDone(res) {
+      // console.log('res:', res);
+      // 例如选中第一条数据
+      if (res.data?.length) {
+        this.$refs.table.setCurrentRow(res.data[0]);
+      }
+    },
+    onSelectionChange(selection) {
+      this.selection = selection;
+    },
+    onCurrentChange(current) {
+      this.current = current;
+      // console.log(current);
+      this.$emit('getCurrent', current);
+    },
+
+    /* 删除数据 */
+    remove(row) {
+      const loading = this.$loading({ lock: true });
+      DeletePlanList(row)
+        .then((res) => {
+          this.$message.success(res.msg);
+          loading.close();
+          // this.$refs.table.reRenderTable();
+          this.reload();
+          this.$forceUpdate();
+        })
+        .catch((err) => {
+          loading.close();
+          this.$message.error(err);
+        });
+    },
+    GetConsume() {
+      var data = {
+        deptTwoCode: this.$store.state.user.info.DeptNow.Dept_Two_Code
+      };
+      SearchHistoryConsumedAndPurchaseDept(data)
+        .then((res) => {
+          this.applyPlanSbz = res.result[0].Purchase_Cost.toFixed(2);
+          this.applyPlanXhz = res.result[0].Consumed_Cost.toFixed(2);
+          if (data.result[0].Purchase_Cost > 0) {
+            this.applyPlanBl =
+              (
+                (data.result[0].Consumed_Cost / data.result[0].Purchase_Cost) *
+                100
+              ).toFixed(2) + '%';
+          } else {
+            this.applyPlanBl = 0 + '%';
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    ReturnStateBtn(data) {
+      // var IDs = data.PlanNum;
+      // var IDStr = IDs + ',';
+      // IDs.forEach((item) => {
+      //   IDStr += item + ',';
+      // });
+      // IDStr.substring(0, IDStr.length - 1);
+
+      var data2 = {
+        IDs: data.PlanNum
+      };
+      const loading = this.$messageLoading('正在保存。。。');
+      ReturnInitState(data2)
+        .then((res) => {
+          loading.close();
+          this.$message.success(res.msg);
+          // this.reload();
+          // this.$forceUpdate();
+          this.key += 1;
+        })
+        .catch((err) => {
+          loading.close();
+          this.$message.error(err);
+        });
+    }
+  },
+  watch: {
+    IsReload() {
+      if (this.IsReload == true) {
+        this.reload(); // 调用刷新表格的方法
+      }
+    }
+  },
+  created() {
+    // this.getdatasource();
+    this.GetConsume();
+  },
+  // 取消监听bus事件
+  destroyed() {
+    // 取消对handleCommand事件的监听
+    this.$bus.$off('handleCommand');
+  }
+};
+</script>
+
+
+<style scoped>
+.ele-body {
+  padding: 0px;
+}
+
+.my-label {
+  background: #53be1d;
+}
+
+.my-content {
+  background: #834646;
+}
+</style>
---
+++ ele-admin-template/src/views/KSInventory/StorageManagementV2/components/KSDepartmentalPlanDetails-search.vue
@@ -0,0 +1,156 @@
+<!-- 搜索表单 -->
+<template>
+  <el-form class="ele-form-search">
+    <el-row :gutter="10" style="display: none">
+      <!-- <el-col v-bind="styleResponsive ? { lg: 6, md: 12 } : { span: 6 }">
+        <el-form-item label="状态：">
+          <el-select v-model="where.State" @change="search()">
+            <el-option label="显示所有申领品种" value="-1"></el-option>
+            <el-option label="仅显示实际申领为空品种" value="0"></el-option>
+            <el-option label="仅显示实际申领非空品种" value="1"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col> -->
+      <el-col v-bind="styleResponsive ? { lg: 11, md: 12 } : { span: 6 }">
+        <el-form-item label="平均用量时间段：" label-width='130px'>
+          <el-date-picker v-model="where.dateFrom" type="date" value-format="yyyy-MM-dd" placeholder="yyyy-MM-dd">
+          </el-date-picker>
+        </el-form-item>
+      </el-col>
+      <el-col v-bind="styleResponsive ? { lg: 4, md: 12 } : { span: 6 }">
+        <el-date-picker v-model="where.dateTo" type="date" value-format="yyyy-MM-dd" placeholder="yyyy-MM-dd">
+        </el-date-picker>
+      </el-col>
+    </el-row>
+    <!-- <el-col :lg="6" :md="12">
+        <el-form-item label="">
+          <el-input v-model="where.SerachName" placeholder="请输入品种名称/品种编码/型号规格/生产企业搜索" clearable />
+        </el-form-item>
+      </el-col> -->
+    <el-row :gutter="10">
+      <el-col :lg="5" :md="12">
+        <el-form-item label="UDI码">
+          <el-input v-model="SerachName" placeholder="" clearable style="width:240px" />
+        </el-form-item>
+      </el-col>
+      <el-col :lg="5" :md="12">
+        <el-form-item label="产品码">
+          <el-input v-model="SerachName2" placeholder="" clearable style="width:240px" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="10">
+      <el-col :lg="5" :md="12">
+        <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+          <el-radio-button :label="true">材料登记</el-radio-button>
+          <el-radio-button :label="false">材料汇总</el-radio-button>
+          <el-radio-button :label="false">收费汇总</el-radio-button>
+        </el-radio-group> -->
+        <el-radio-group v-model="radio1">
+          <el-radio-button label="入库耗材登记"></el-radio-button>
+          <!-- <el-radio-button label="材料汇总"></el-radio-button>
+          <el-radio-button label="收费汇总"></el-radio-button> -->
+        </el-radio-group>
+      </el-col>
+      <!-- <el-col :lg="18" :md="12">
+        <p style="display: flex;align-items: center;justify-content: flex-end;">
+          合计使用鼓量: <b>2 </b>
+          材料合计: <b>2 </b>
+          未收费材料: <b>0.00 </b>
+          收费材料: <b>140.00 </b>
+          收费合计: <b>140.00 </b>
+          供应商结算金额: <b>140.00</b></p>
+      </el-col> -->
+    </el-row>
+
+    <IntroduceUserDefinedTemp :visible.sync="showEdit" :IntroduceUserDefinedTempSearch="KSDepartmentalPlanDataSearch" />
+    <IntroduceDefinedTemp :visible.sync="showEdit2" />
+    <BidVarInfoDept :visible.sync="BidListShowEdit" />
+    <ApplyOperateTip :visible.sync="ApplyOperateTipShow" />
+    <ApplyOperateTip :visible.sync="ApplyOperateTipShow" />
+    <VarietyDataLzhLook :visible.sync="VarietyDataLzhLookShow" />
+    <DpetOneAuthWithDept :visible.sync="DpetOneAuthWithDeptShow" />
+    <el-dialog title="授权品种目录" :visible.sync="ApplyTempPage" width='95%'>
+      <!-- <AuthVarTable :dialogTableVisible="dialogTableVisible" :ApplyTempTableDataID="ApplyTempTableDataID" /> -->
+      <ApplyTemp :IntroduceUserDefinedTempSearch="KSDepartmentalPlanDataSearch" @ApplyTempPageChange="ApplyTempPageChange" />
+    </el-dialog>
+
+    <el-dialog title="提示" :visible.sync="centerDialogVisible" width="30%" center>
+      <span>存在申领数量为0的明细</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="deleteZeroDelAndCommit">剔除并提交</el-button>
+        <el-button @click="deleteZeroDelAndCommit2">取 消</el-button>
+      </span>
+    </el-dialog>
+  </el-form>
+</template>
+
+<script>
+import { API_BASE_URL, BACK_BASE_URL } from '@/config/setting';
+import { reloadPageTab, finishPageTab } from '@/utils/page-tab-util';
+import {
+  DeletePlanDeta,
+  PutInListDeta,
+  ToExamine,
+  KeeptListDeta,
+  isHaveZeroDel,
+  deleteZeroDel
+} from '@/api/KSInventory/KSDepartmentalPlan';
+import IntroduceUserDefinedTemp from '@/views/KSInventory/IntroduceUserDefinedTemp/index.vue';
+import BidVarInfoDept from '@/views/KSInventory/ReferenceComponent/BidVarInfoDept/index.vue';
+import ApplyOperateTip from '@/views/KSInventory/ReferenceComponent/ApplyOperateTip/index.vue';
+import VarietyDataLzhLook from '@/views/KSInventory/ReferenceComponent/VarietyDataLzhLook/index.vue';
+import DpetOneAuthWithDept from '@/views/KSInventory/ReferenceComponent/DpetOneAuthWithDept/index.vue';
+import ApplyTemp from '@/views/KSInventory/ApplyTemp/index.vue';
+import IntroduceDefinedTemp from './aaaaccc.vue';
+export default {
+  props: ['KSDepartmentalPlanDataSearch', 'selection', 'datasourceList'],
+  components: {
+    IntroduceUserDefinedTemp,
+    IntroduceDefinedTemp,
+    ApplyTemp,
+    BidVarInfoDept,
+    ApplyOperateTip,
+    VarietyDataLzhLook,
+    DpetOneAuthWithDept
+  },
+  data() {
+    // 默认表单数据
+    const defaultWhere = {
+      Token: '',
+      PlanNum: '',
+      is_second_app: '',
+      SerachName: '',
+      SerachName2: '',
+      dateFrom: '',
+      dateTo: ''
+    };
+    return {
+      // 表单数据
+      where: { ...defaultWhere },
+      showEdit: false,
+      showEdit2: false,
+      ApplyTempPage: false,
+      centerDialogVisible: false,
+      BidListShowEdit: false,
+      ApplyOperateTipShow: false,
+      VarietyDataLzhLookShow: false,
+      DpetOneAuthWithDeptShow: false,
+      HidesubToExamine: false,
+      visibleLine: 'none',
+      isCollapse: true,
+      radio1: '入库耗材登记'
+    };
+  },
+  computed: {
+    // 是否开启响应式布局
+    styleResponsive() {
+      return this.$store.state.theme.styleResponsive;
+    }
+  },
+  watch: {},
+  methods: {},
+  created() {}
+};
+</script>
