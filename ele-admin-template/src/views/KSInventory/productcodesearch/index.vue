628437063eabccc58bb62aa53d574a190b9896ef
liao <2414690715@qq.com>
2024-06-11 16:48:55 +0800

    fix

--- ele-admin-template/src/views/KSInventory/productcodesearch/index.vue
+++ ele-admin-template/src/views/KSInventory/productcodesearch/index.vue
@@ -45,12 +45,12 @@
         <el-main style="padding-top: 20px">
             <el-card shadow="always">
                 <el-steps :active="2" align-center>
-                    <el-step title="采购入库" description="2024-05-16 09:01:18"></el-step>
+                    <el-step title="采购入库" description="2024-05-16"></el-step>
                     <!-- <el-step title="扫码出库" description="王凯锋 2023-11-10"></el-step> -->
-                    <el-step title="扫码出库" description="王凯锋 2024-05-16 09:01:18"></el-step>
-                    <el-step title="科室入库" description="王凯锋 2024-05-23 15:33:10"></el-step>
-                    <el-step title="科室领出" description=""></el-step>
-                    <el-step title="科室消耗" description=""></el-step>
+                    <el-step title="扫码出库" description="王凯锋 2024-05-16"></el-step>
+                    <el-step title="复合" description="晏江彬 2024-05-17"></el-step>
+                    <el-step title="配送调度" description="粤B23B7B 2024-05-18"></el-step>
+                    <el-step title="医院签收" description="2024-05-19"></el-step>
                 </el-steps>
             </el-card>
         </el-main>
@@ -74,6 +74,13 @@ export default {
                 {
                     prop: 'CURRENT_STATE',
                     label: '当前状态',
+                    align: 'center',
+                    showOverflowTooltip: true,
+                    minWidth: 110,
+                },
+                {
+                    prop: 'PEOPLE_NUM',
+                    label: '病患身份证号',
                     align: 'center',
                     showOverflowTooltip: true,
                     minWidth: 110,
@@ -171,24 +178,12 @@ export default {
                 "total": 31,
                 "result": [
                     {
-                        "CURRENT_STATE": "物资消耗",
+                        "CURRENT_STATE": "医院签收",
                         "CODE": "01008858360711611116103117260930102869847",
                         "PRODUCT_NAME": "髋关节假体-陶瓷股骨头和衬垫-7927-Bioloxdelta股骨头",
                         "PLATFORM": "1个/盒",
                         "PC_NUM": "2869847",
-                        "LIVE_DATE": "2026-09-30",
-                        "UNIT": "个",
-                        "NUM": 1,
-                        "PRICE": "1984.00",
-                        "TOTAL_PRICE": "1984.00",
-                        "BELONG": "高值",
-                    },
-                    {
-                        "CURRENT_STATE": "物资入库",
-                        "CODE": "01008858360711611116103117260930102869847",
-                        "PRODUCT_NAME": "髋关节假体-陶瓷股骨头和衬垫-7927-Bioloxdelta股骨头",
-                        "PLATFORM": "1个/盒",
-                        "PC_NUM": "2869847",
+                        "PEOPLE_NUM":"未回传",
                         "LIVE_DATE": "2026-09-30",
                         "UNIT": "个",
                         "NUM": 1,
