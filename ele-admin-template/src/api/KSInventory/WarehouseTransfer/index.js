8f19d894fd4901bfe4d58e88f94f653fd100832b
liao <2414690715@qq.com>
2024-08-12 14:10:06 +0800

    fix(调库主单API)

--- ele-admin-template/src/api/KSInventory/WarehouseTransfer/index.js
+++ ele-admin-template/src/api/KSInventory/WarehouseTransfer/index.js
@@ -88,8 +88,10 @@ export async function getDEPT_TK_MAIN(data) {
     data2.Token = Token;
     data2.page = data.page;
     data2.size = data.limit;
-    data2.TK_MAIN_ID = data.TK_MAIN_ID ? data.TK_MAIN_ID : '';
-    data2.TK_DEL_ID = data.TK_DEL_ID ? data.TK_DEL_ID : '';
+    data2.TK_MAIN_ID = data.where.TK_MAIN_ID ? data.where.TK_MAIN_ID : '';
+    data2.TK_DEL_ID = data.where.TK_DEL_ID ? data.where.TK_DEL_ID : '';
+    // data2.TK_MAIN_ID = '';
+    // data2.TK_DEL_ID = '4';
     let data3 = formdataify(data2);
     const res = await request.post('/DeptConsume/getDEPT_TK_DEF', data3);
     if (res.data.code == 200) {
@@ -122,6 +124,54 @@ export async function getDEPT_TK_MAIN(data) {
     data2.size = data.limit;
     let data3 = formdataify(data2);
     const res = await request.post('/DeptConsume/chooseDefPkgTableList', data3);
+    if (res.data.code == 200) {
+      return res.data;
+    }
+    return Promise.reject(new Error(res.data.msg));
+  }
+
+  export async function createDEPT_TK_VAR(data) {
+    var Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+    var data2 = {}
+    //添加参数
+    data2.Token = Token;
+    data2.TK_MAIN_ID = data.TK_MAIN_ID;
+    data2.json = data.json;
+    let data3 = formdataify(data2);
+    const res = await request.post('/DeptConsume/createDEPT_TK_VAR', data3);
+    if (res.data.code == 200) {
+      return res.data;
+    }
+    return Promise.reject(new Error(res.data.msg));
+  }
+
+
+  export async function itemDefPkgTableList(data) {
+    var Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+    var data2 = {}
+    //添加参数
+    data2.Token = Token;
+    data2.deptCode = store.state.user.info.DeptNow.Dept_Two_Code
+    data2.code = data.where.code;
+    data2.page = data.page;
+    data2.size = data.limit;
+    let data3 = formdataify(data2);
+    const res = await request.post('/DeptConsume/itemDefPkgTableList', data3);
+    if (res.data.code == 200) {
+      return res.data;
+    }
+    return Promise.reject(new Error(res.data.msg));
+  }
+
+  export async function createDEPT_TK_DEF(data) {
+    var Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+    var data2 = {}
+    //添加参数
+    data2.Token = Token;
+    data2.TK_DEL_ID = data.id;
+    data2.json = data.json;
+    let data3 = formdataify(data2);
+    const res = await request.post('/DeptConsume/createDEPT_TK_DEF', data3);
     if (res.data.code == 200) {
       return res.data;
     }
