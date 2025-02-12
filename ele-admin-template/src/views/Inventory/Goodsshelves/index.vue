91e03bc393da58003791a1b6ce9c102d87d49ae4
zhb <zouhaibing1@gmail.com>
2025-02-12 10:16:40 +0800

    库存管理文件

---
+++ ele-admin-template/src/views/Inventory/Goodsshelves/index.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="ele-body">
+    <el-card shadow="never">
+      <!-- 搜索表单 -->
+      <!-- <user-search @search="reload" @exportData="exportData" /> -->
+      <!-- 数据表格 -->
+      <user-search @search="reload" @exportData="exportData" />
+      <ele-pro-table ref="table" :pageSize="pageSize" :pageSizes="pageSizes" :columns="columns" :datasource="datasource" :selection.sync="selection" cache-key="KSInventoryBasicDataTable">
+        <!-- 表头工具栏 -->
+        <template v-slot:toolbar>
+        </template>
+
+        <!-- 操作列 -->
+        <template v-slot:action="{ row }">
+          <el-link type="primary" :underline="false" icon="el-icon-edit" @click="openEdit(row)">
+            修改
+          </el-link>
+          <!-- <el-button type="primary" size="mini" @click="openEdit(row)">编辑</el-button> -->
+          <!-- <el-popconfirm class="ele-action" title="确定要删除此用户吗？" @confirm="remove(row)">
+            <template v-slot:reference>
+              <el-link type="danger" :underline="false" icon="el-icon-delete">
+                删除
+              </el-link>
+            </template>
+          </el-popconfirm> -->
+        </template>
+      </ele-pro-table>
+    </el-card>
+    <!-- 编辑弹窗 -->
+    <user-edit :visible.sync="showEdit" :data="current" @done="reload" />
+    <!-- 导入弹窗 -->
+    <!-- <user-import :visible.sync="showImport" @done="reload" /> -->
+  </div>
+</template>
+
+<script>
+import { utils, writeFile } from 'xlsx';
+import UserSearch from './components/user-search.vue';
+import {
+  GetPDAList,
+} from '@/api/KSInventory/InstrumentalAnalysis';
+export default {
+  name: 'SystemUser',
+  components: {
+    UserSearch,
+   
+  },
+  data() {
+    return {
+      // 表格列配置
+      columns: [
+        {
+          columnKey: 'selection',
+          type: 'selection',
+          width: 45,
+          align: 'center',
+          fixed: 'left'
+        },
+        {
+          label: '序',
+          columnKey: 'index',
+          type: 'index',
+          width: 45,
+          align: 'center',
+          showOverflowTooltip: true,
+          fixed: 'left'
+        },
+        // {
+        //   columnKey: 'action',
+        //   label: '操作',
+        //   width: 120,
+        //   align: 'center',
+        //   resizable: false,
+        //   slot: 'action',
+        //   showOverflowTooltip: true,
+        //   fixed: 'left'
+        // },
+        {
+          prop: 'BIND_MACHINE',
+          label: '机械号',
+          sortable: 'custom',
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 150
+        },
+        {
+          prop: 'VARIETIE_CODE_NEW',
+          label: '品种编码',
+          sortable: 'custom',
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 150,
+          show: false
+        },
+        {
+          prop: 'VARIETIE_NAME',
+          label: '品种名称',
+          sortable: 'custom',
+          align: 'center',
+          howOverflowTooltip: true,
+          minWidth: 180
+        },
+        {
+          prop: 'SPECIFICATION_OR_TYPE',
+          label: '规格/型号',
+          sortable: 'custom',
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 200
+        },
+        {
+          prop: 'Manufacturing_Ent_Name',
+          label: '生产企业名称',
+          sortable: 'custom',
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 180
+        },
+        {
+          prop: 'UNIT',
+          label: '单位',
+          sortable: 'custom',
+          align: 'center',
+          showOverflowTooltip: true,
+          minWidth: 180
+        },
+        {
+          prop: 'SUM',
+          label: '数量',
+          align: 'center',
+          sortable: 'custom',
+          showOverflowTooltip: true,
+          minWidth: 80
+        },
+        {
+          prop: 'LIS_COUNT',
+          label: 'PAD扫码数量',
+          align: 'center',
+          sortable: 'custom',
+          width: 120,
+          showOverflowTooltip: true
+        },
+      ],
+      toolbar: false,
+      pageSize: 10,
+      pageSizes: [10, 20, 50, 100, 9999999],
+      pagerCount: 5,
+      // 表格选中数据
+      selection: [],
+      // 当前编辑数据
+      current: null,
+      // 是否显示编辑弹窗
+      showEdit: false,
+      // 是否显示导入弹窗
+      showImport: false,
+      // datasource: [],
+      data: []
+    };
+  },
+  methods: {
+    /* 表格数据源 */
+    datasource({ page, limit, where, order }) {
+      let data = GetPDAList({ page, limit, where, order }).then(
+        (res) => {
+          return res.result;
+        }
+      );
+      return data;
+    },
+    /* 刷新表格 */
+    reload(where) {
+      this.$refs.table.reload({ page: 1, where: where });
+    },
+    /* 打开编辑弹窗 */
+    openEdit(row) {
+      this.current = row;
+      this.showEdit = true;
+    },
+    /* 打开导入弹窗 */
+    openImport() {
+      this.showImport = true;
+    },
+    exportData(data) {
+      const loading = this.$messageLoading('正在导出数据...');
+      this.$refs.table.doRequest(({ where, order }) => {
+        where = data;
+        where.Dept_One_Code = this.$store.state.user.info.DeptNow.Dept_Two_Code;
+        GetPDAList({
+          page: 1,
+          limit: 999999,
+          where: where,
+          order: order
+        })
+          .then((res) => {
+            loading.close();
+            const array = [
+              [
+                '品种编码',
+                '品种id',
+                '品种名称',
+                '规格/型号',
+                '生产企业名称',
+                '注册证号',
+                '单位',
+                '中标价',
+                '品种类别',
+                '换算比(试剂)',
+                '仪器备注',
+              ]
+            ];
+            res.result.forEach((d) => {
+              array.push([
+                d.Varietie_Code_New,
+                d.Varietie_Code,
+                d.Varietie_Name,
+                d.Specification_Or_Type,
+                d.Manufacturing_Ent_Name,
+                d.APPROVAL_NUMBER,
+                d.UNIT,
+                d.Price ,
+                d.CLASS_NUM,
+                d.CONVERSION_RATIO,
+                d.DEVICE_REMARK,
+                // this.$util.toDateString(d.createTime)
+              ]);
+            });
+            writeFile(
+              {
+                SheetNames: ['Sheet1'],
+                Sheets: {
+                  Sheet1: utils.aoa_to_sheet(array)
+                }
+              },
+              '科室入库品种.xlsx'
+            );
+            this.$message.success("导出成功");
+          })
+          .catch((e) => {
+            loading.close();
+            this.$message.error(e.message);
+          });
+      });
+    }
+  },
+  created() {
+    // this.getdatasource();
+    // console.log(this.$store.state.user.info)
+  }
+};
+</script>
