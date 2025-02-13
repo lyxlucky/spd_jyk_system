b66273156003ea95a5692d459c267a77befce89b
zhb <zouhaibing1@gmail.com>
2025-02-13 14:43:57 +0800

    物价节点文件

--- ele-admin-template/src/views/MedicalInsuranceAudit/PriceFlowNode/components/KSDepartmentalPlanDetails-table2.vue
+++ ele-admin-template/src/views/MedicalInsuranceAudit/PriceFlowNode/components/KSDepartmentalPlanDetails-table2.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="ele-body">
     <el-button size="mini" icon="el-icon-refresh" @click="reset">重置</el-button>
+    <div style="height: 300px;">
+      <el-steps direction="vertical" :active="1">
+        <el-step title="步骤 1"></el-step>
+        <el-step title="步骤 2"></el-step>
+        <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
+      </el-steps>
+    </div>
   </div>
 </template>
 
