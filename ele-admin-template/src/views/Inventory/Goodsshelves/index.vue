5a3516e1620d13597050dd7edd2a48209d5cbbc4
lzhua <958875578@qq.com>
2025-03-20 20:53:51 +0800

    流向查询

--- ele-admin-template/src/views/Inventory/Goodsshelves/index.vue
+++ ele-admin-template/src/views/Inventory/Goodsshelves/index.vue
@@ -1,250 +1,66 @@
 <template>
-  <div class="ele-body">
-    <el-card shadow="never">
-      <!-- 搜索表单 -->
-      <!-- <user-search @search="reload" @exportData="exportData" /> -->
-      <!-- 数据表格 -->
-      <user-search @search="reload" @exportData="exportData" />
-      <ele-pro-table ref="table" :pageSize="pageSize" :pageSizes="pageSizes" :columns="columns" :datasource="datasource" :selection.sync="selection" cache-key="KSInventoryBasicDataTable">
-        <!-- 表头工具栏 -->
-        <template v-slot:toolbar>
-        </template>
-
-        <!-- 操作列 -->
-        <template v-slot:action="{ row }">
-          <el-link type="primary" :underline="false" icon="el-icon-edit" @click="openEdit(row)">
-            修改
-          </el-link>
-          <!-- <el-button type="primary" size="mini" @click="openEdit(row)">编辑</el-button> -->
-          <!-- <el-popconfirm class="ele-action" title="确定要删除此用户吗？" @confirm="remove(row)">
-            <template v-slot:reference>
-              <el-link type="danger" :underline="false" icon="el-icon-delete">
-                删除
-              </el-link>
-            </template>
-          </el-popconfirm> -->
-        </template>
-      </ele-pro-table>
-    </el-card>
-    <!-- 编辑弹窗 -->
-    <user-edit :visible.sync="showEdit" :data="current" @done="reload" />
-    <!-- 导入弹窗 -->
-    <!-- <user-import :visible.sync="showImport" @done="reload" /> -->
+  <div>
+    <!-- 表头 -->
+    <div class="header">
+      <span
+        @click="showLeftPage = true; showRightPage = false"
+        :class="{ 'active': showLeftPage }"
+      >
+      中心库入库查询
+      </span>
+      <span
+        @click="showRightPage = true; showLeftPage = false"
+        :class="{ 'active': showRightPage }"
+      >
+      中心库出库查询
+      </span>
+    </div>
+    <!-- 根据条件显示不同组件 -->
+    <LeftPage v-if="showLeftPage" />
+    <RightPage v-if="showRightPage" />
   </div>
 </template>
 
 <script>
-import { utils, writeFile } from 'xlsx';
-import UserSearch from './components/user-search.vue';
-import {
-  GetPDAList,
-} from '@/api/KSInventory/InstrumentalAnalysis';
+import LeftPage from '../../Inventory/Goodsshelves/components/LeftPage.vue';
+import RightPage from '../../Inventory/Goodsshelves/components/RightPage.vue';
+
 export default {
-  name: 'SystemUser',
   components: {
-    UserSearch,
-   
+    LeftPage,
+    RightPage
   },
   data() {
     return {
-      // 表格列配置
-      columns: [
-        {
-          columnKey: 'selection',
-          type: 'selection',
-          width: 45,
-          align: 'center',
-          fixed: 'left'
-        },
-        {
-          label: '序',
-          columnKey: 'index',
-          type: 'index',
-          width: 45,
-          align: 'center',
-          showOverflowTooltip: true,
-          fixed: 'left'
-        },
-        // {
-        //   columnKey: 'action',
-        //   label: '操作',
-        //   width: 120,
-        //   align: 'center',
-        //   resizable: false,
-        //   slot: 'action',
-        //   showOverflowTooltip: true,
-        //   fixed: 'left'
-        // },
-        {
-          prop: 'BIND_MACHINE',
-          label: '机械号',
-          sortable: 'custom',
-          align: 'center',
-          showOverflowTooltip: true,
-          minWidth: 150
-        },
-        {
-          prop: 'VARIETIE_CODE_NEW',
-          label: '品种编码',
-          sortable: 'custom',
-          align: 'center',
-          showOverflowTooltip: true,
-          minWidth: 150,
-          show: false
-        },
-        {
-          prop: 'VARIETIE_NAME',
-          label: '品种名称',
-          sortable: 'custom',
-          align: 'center',
-          howOverflowTooltip: true,
-          minWidth: 180
-        },
-        {
-          prop: 'SPECIFICATION_OR_TYPE',
-          label: '规格/型号',
-          sortable: 'custom',
-          align: 'center',
-          showOverflowTooltip: true,
-          minWidth: 200
-        },
-        {
-          prop: 'Manufacturing_Ent_Name',
-          label: '生产企业名称',
-          sortable: 'custom',
-          align: 'center',
-          showOverflowTooltip: true,
-          minWidth: 180
-        },
-        {
-          prop: 'UNIT',
-          label: '单位',
-          sortable: 'custom',
-          align: 'center',
-          showOverflowTooltip: true,
-          minWidth: 180
-        },
-        {
-          prop: 'SUM',
-          label: '数量',
-          align: 'center',
-          sortable: 'custom',
-          showOverflowTooltip: true,
-          minWidth: 80
-        },
-        {
-          prop: 'LIS_COUNT',
-          label: 'PAD扫码数量',
-          align: 'center',
-          sortable: 'custom',
-          width: 120,
-          showOverflowTooltip: true
-        },
-      ],
-      toolbar: false,
-      pageSize: 10,
-      pageSizes: [10, 20, 50, 100, 9999999],
-      pagerCount: 5,
-      // 表格选中数据
-      selection: [],
-      // 当前编辑数据
-      current: null,
-      // 是否显示编辑弹窗
-      showEdit: false,
-      // 是否显示导入弹窗
-      showImport: false,
-      // datasource: [],
-      data: []
+      showLeftPage: true,
+      showRightPage: false
     };
-  },
-  methods: {
-    /* 表格数据源 */
-    datasource({ page, limit, where, order }) {
-      let data = GetPDAList({ page, limit, where, order }).then(
-        (res) => {
-          return res.result;
-        }
-      );
-      return data;
-    },
-    /* 刷新表格 */
-    reload(where) {
-      this.$refs.table.reload({ page: 1, where: where });
-    },
-    /* 打开编辑弹窗 */
-    openEdit(row) {
-      this.current = row;
-      this.showEdit = true;
-    },
-    /* 打开导入弹窗 */
-    openImport() {
-      this.showImport = true;
-    },
-    exportData(data) {
-      const loading = this.$messageLoading('正在导出数据...');
-      this.$refs.table.doRequest(({ where, order }) => {
-        where = data;
-        where.Dept_One_Code = this.$store.state.user.info.DeptNow.Dept_Two_Code;
-        GetPDAList({
-          page: 1,
-          limit: 999999,
-          where: where,
-          order: order
-        })
-          .then((res) => {
-            loading.close();
-            const array = [
-              [
-                '品种编码',
-                '品种id',
-                '品种名称',
-                '规格/型号',
-                '生产企业名称',
-                '注册证号',
-                '单位',
-                '中标价',
-                '品种类别',
-                '换算比(试剂)',
-                '仪器备注',
-              ]
-            ];
-            res.result.forEach((d) => {
-              array.push([
-                d.Varietie_Code_New,
-                d.Varietie_Code,
-                d.Varietie_Name,
-                d.Specification_Or_Type,
-                d.Manufacturing_Ent_Name,
-                d.APPROVAL_NUMBER,
-                d.UNIT,
-                d.Price ,
-                d.CLASS_NUM,
-                d.CONVERSION_RATIO,
-                d.DEVICE_REMARK,
-                // this.$util.toDateString(d.createTime)
-              ]);
-            });
-            writeFile(
-              {
-                SheetNames: ['Sheet1'],
-                Sheets: {
-                  Sheet1: utils.aoa_to_sheet(array)
-                }
-              },
-              '科室入库品种.xlsx'
-            );
-            this.$message.success("导出成功");
-          })
-          .catch((e) => {
-            loading.close();
-            this.$message.error(e.message);
-          });
-      });
-    }
-  },
-  created() {
-    // this.getdatasource();
-    // console.log(this.$store.state.user.info)
   }
 };
 </script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #ccc; /* 整体表头底部的分割线 */
+}
+
+.header span {
+  flex: 1; /* 让每个表头元素各占一半宽度 */
+  text-align: center;
+  padding: 10px 0;
+  cursor: pointer;
+  position: relative;
+}
+
+.header span.active::after {
+  content: '';
+  position: absolute;
+  bottom: -1px; /* 与表头底部对齐 */
+  left: 0;
+  right: 0;
+  height: 2px;
+  background-color: #007bff; /* 选中状态下滑线颜色 */
+}
+</style>
