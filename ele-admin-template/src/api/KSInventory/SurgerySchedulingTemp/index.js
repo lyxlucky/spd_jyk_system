f23060b2116df05ef581d529a2ecae40e3f56239
zhb <zouhaibing1@gmail.com>
2025-03-26 11:09:49 +0800

    z1

--- ele-admin-template/src/api/KSInventory/SurgerySchedulingTemp/index.js
+++ ele-admin-template/src/api/KSInventory/SurgerySchedulingTemp/index.js
@@ -39,11 +39,12 @@ export async function GetNaxtDayApplyPlanTemp(data) {
 }
 
 export async function GetNaxtDayApplyPlanDelTemp(data) {
+    console.log(data)
     var data2 = {};
     data2.VARIETIE_NAME = data.where.VARIETIE_NAME ? data.where.VARIETIE_NAME : '';
     data2.MAIN_ID = data.where.MAIN_ID ? data.where.MAIN_ID : '';
-    data2.page = data.where.page ? data.where.page : '1';
-    data2.size = data.where.limit ? data.where.limit : '10';
+    data2.page = data.page ? data.page : '1';
+    data2.size = data.limit ? data.limit : '10';
     data2.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
 
     var rep = formdataify(data2);
