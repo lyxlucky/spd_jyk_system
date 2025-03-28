440a5a93659f2e3b75dec7b4b94248de8ab83561
liao <email@lyxlucky.me>
2025-03-28 12:31:29 +0800

    fix

--- ele-admin-template/src/api/Home/masterBaseData/index.js
+++ ele-admin-template/src/api/Home/masterBaseData/index.js
@@ -1,78 +1,115 @@
-
 import request from '@/utils/request';
 import { formdataify, formdataifyGet } from '@/utils/formdataify';
-import { TOKEN_STORE_NAME } from '@/config/setting'
-
-
+import { TOKEN_STORE_NAME } from '@/config/setting';
 
 export async function GetSpdMainsjHeaderIface(data) {
-    let formataData = {}
-    formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
-    formataData.page = data.page ? data.page : '1';
-    formataData.size = data.size ? data.size : '10';
-    formataData.PROCESS_STATUS = data.where.PROCESS_STATUS ? data.where.PROCESS_STATUS : '';
-    formataData.REQUESTNOTEID = data.where.REQUESTNOTEID ? data.where.REQUESTNOTEID : '';
-    formataData.APPLYDEPT = data.where.APPLYDEPT ? data.where.APPLYDEPT : '';
-    formataData.APPLYPEOPLE = data.where.APPLYPEOPLE ? data.where.APPLYPEOPLE : '';
-    formataData.YB_SP_STATE = data.where.YB_SP_STATE ? data.where.YB_SP_STATE : '';
-    formataData.WJ_SP_STATE = data.where.WJ_SP_STATE ? data.where.WJ_SP_STATE : '';
+  let formataData = {};
+  formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+  formataData.page = data.page ? data.page : '1';
+  formataData.size = data.size ? data.size : '10';
+  formataData.PROCESS_STATUS = data.where.PROCESS_STATUS
+    ? data.where.PROCESS_STATUS
+    : '';
+  formataData.REQUESTNOTEID = data.where.REQUESTNOTEID
+    ? data.where.REQUESTNOTEID
+    : '';
+  formataData.APPLYDEPT = data.where.APPLYDEPT ? data.where.APPLYDEPT : '';
+  formataData.APPLYPEOPLE = data.where.APPLYPEOPLE
+    ? data.where.APPLYPEOPLE
+    : '';
+  formataData.YB_SP_STATE = data.where.YB_SP_STATE
+    ? data.where.YB_SP_STATE
+    : '';
+  formataData.WJ_SP_STATE = data.where.WJ_SP_STATE
+    ? data.where.WJ_SP_STATE
+    : '';
 
-    console.log(formataData)
-    let req = formdataify(formataData);
-    const res = await request.post('/AJykDept/GetSpdMainsjHeaderIface', req)
-    if (res.data.code == 200) {
-        return res.data;
-    } else {
-        return Promise.reject(new Error(res.data.msg));
-    }
+  console.log(formataData);
+  let req = formdataify(formataData);
+  const res = await request.post('/AJykDept/GetSpdMainsjHeaderIface', req);
+  if (res.data.code == 200) {
+    return res.data;
+  } else {
+    return Promise.reject(new Error(res.data.msg));
+  }
 }
 
 export async function GetSpdMainsjLinesIface(data) {
-    let formataData = {}
-    formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
-    formataData.page = data.page ? data.page : '1';
-    formataData.size = data.size ? data.size : '10';
-    formataData.HEADER_IFACE_ID = data.where.HEADER_IFACE_ID ? data.where.HEADER_IFACE_ID : '-1';
-    formataData.REGISTRATION_NAME = data.where.REGISTRATION_NAME ? data.where.REGISTRATION_NAME : '';
-    formataData.REGISTRATION_NO = data.where.REGISTRATION_NO ? data.where.REGISTRATION_NO : '';
-    formataData.ITEM_NUMBER = data.where.ITEM_NUMBER ? data.where.ITEM_NUMBER : '';
-    formataData.STAND_VALUE = data.where.STAND_VALUE ? data.where.STAND_VALUE : '';
+  let formataData = {};
+  formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+  formataData.page = data.page ? data.page : '1';
+  formataData.size = data.size ? data.size : '10';
+  formataData.HEADER_IFACE_ID = data.where.HEADER_IFACE_ID
+    ? data.where.HEADER_IFACE_ID
+    : '-1';
+  formataData.REGISTRATION_NAME = data.where.REGISTRATION_NAME
+    ? data.where.REGISTRATION_NAME
+    : '';
+  formataData.REGISTRATION_NO = data.where.REGISTRATION_NO
+    ? data.where.REGISTRATION_NO
+    : '';
+  formataData.ITEM_NUMBER = data.where.ITEM_NUMBER
+    ? data.where.ITEM_NUMBER
+    : '';
+  formataData.STAND_VALUE = data.where.STAND_VALUE
+    ? data.where.STAND_VALUE
+    : '';
 
-    console.log(formataData)
-    let req = formdataify(formataData);
-    const res = await request.post('/AJykDept/GetSpdMainsjLinesIface', req)
-    if (res.data.code == 200) {
-        return res.data;
-    } else {
-        return Promise.reject(new Error(res.data.msg));
-    }
+  console.log(formataData);
+  let req = formdataify(formataData);
+  const res = await request.post('/AJykDept/GetSpdMainsjLinesIface', req);
+  if (res.data.code == 200) {
+    return res.data;
+  } else {
+    return Promise.reject(new Error(res.data.msg));
+  }
 }
 
 export async function ImportSpdMainsjLinesIfaceExcel(data) {
-    data.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
-    const res = await request.post('/AJykDept/ImportSpdMainsjLinesIfaceExcel', data);
-    if (res.data.code == 200) {
-        return res.data;
-    } else {
-        return Promise.reject(new Error(res.data.msg));
-    }
+  data.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+  const res = await request.post(
+    '/AJykDept/ImportSpdMainsjLinesIfaceExcel',
+    data
+  );
+  if (res.data.code == 200) {
+    return res.data;
+  } else {
+    return Promise.reject(new Error(res.data.msg));
+  }
 }
-
 
 export async function ApproveYBSpdMainsjLinesIface(data) {
-    let formataData = {}
-    formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
-    formataData.YB_SP_MAN = data.YB_SP_MAN ? data.YB_SP_MAN : '';
-    formataData.YB_SP_MARK = data.YB_SP_MARK ? data.YB_SP_MARK : '';
-    formataData.HEADER_IFACE_ID = data.HEADER_IFACE_ID ? data.HEADER_IFACE_ID : '';
+  let formataData = {};
+  formataData.Token = sessionStorage.getItem(TOKEN_STORE_NAME);
+  formataData.YB_SP_MAN = data.YB_SP_MAN ? data.YB_SP_MAN : '';
+  formataData.YB_SP_MARK = data.YB_SP_MARK ? data.YB_SP_MARK : '';
+  formataData.HEADER_IFACE_ID = data.HEADER_IFACE_ID
+    ? data.HEADER_IFACE_ID
+    : '';
 
-    let req = formdataify(formataData);
-    const res = await request.post('/AJykDept/ApproveYBSpdMainsjLinesIface', req);
-    console.log(res)
-    if (res.data.code == 200) {
-        return res.data;
-    } else {
-        return Promise.reject(new Error(res.data.msg));
-    }
+  let req = formdataify(formataData);
+  const res = await request.post('/AJykDept/ApproveYBSpdMainsjLinesIface', req);
+  console.log(res);
+  if (res.data.code == 200) {
+    return res.data;
+  } else {
+    return Promise.reject(new Error(res.data.msg));
+  }
 }
 
+export async function BatchPushChargeItemToHis(data) {
+  const formData = {
+    Token: sessionStorage.getItem(TOKEN_STORE_NAME),
+    HeaderIfaceId: data.HEADER_IFACE_ID
+  };
+  const res = await request.post(
+    '/PekingApplication/BatchPushChargeItemToHis',
+    formData
+  );
+  console.log(res);
+  if (res.data.code == 200) {
+    return res.data;
+  } else {
+    return Promise.reject(new Error(res.data.msg));
+  }
+}
--- ele-admin-template/src/views/Home/masterBaseData/components/user-edit.vue
+++ ele-admin-template/src/views/Home/masterBaseData/components/user-edit.vue
@@ -1,18 +1,39 @@
 <!-- 用户编辑弹窗 -->
 <template>
-  <ele-modal width="600px" :visible="visible" :append-to-body="true" :close-on-click-modal="true" custom-class="ele-dialog-form" title="修改品种" @update:visible="updateVisible">
+  <ele-modal
+    width="600px"
+    :visible="visible"
+    :append-to-body="true"
+    :close-on-click-modal="true"
+    custom-class="ele-dialog-form"
+    title="修改品种"
+    @update:visible="updateVisible"
+  >
     <el-form ref="form" :model="form" :rules="rules" label-width="82px">
       <el-row :gutter="12">
         <el-col v-bind="styleResponsive ? { sm: 20 } : { span: 20 }">
           <el-form-item label="备注:" prop="WJ_SP_MARK">
-            <el-input size="mini" clearable v-model="form.YB_SP_MARK" placeholder="备注" />
+            <el-input
+              size="mini"
+              clearable
+              v-model="form.YB_SP_MARK"
+              placeholder="备注"
+            />
           </el-form-item>
         </el-col>
       </el-row>
     </el-form>
     <template v-slot:footer>
-      <el-button size="mini" icon="el-icon-close" @click="updateVisible(false)">取消</el-button>
-      <el-button size="mini" icon="el-icon-check" type="primary" :loading="loading" @click="save">
+      <el-button size="mini" icon="el-icon-close" @click="updateVisible(false)"
+        >取消</el-button
+      >
+      <el-button
+        size="mini"
+        icon="el-icon-check"
+        type="primary"
+        :loading="loading"
+        @click="save"
+      >
         保存
       </el-button>
     </template>
@@ -20,121 +41,153 @@
 </template>
 
 <script>
-import { ApproveYBSpdMainsjLinesIface } from '@/api/Home/masterBaseData';
-export default {
-  components: {},
-  props: {
-    // 弹窗是否打开
-    visible: Boolean,
-    // 修改回显的数据
-    data: Array
-  },
-  data() {
-    const defaultForm = {
-      YB_SP_MARK: ''
-    };
-    return {
-      defaultForm,
-      // 表单数据
-      form: { ...defaultForm },
-      options: [],
-      // 表单验证规则
-      rules: {
-        // CONVERSION_RATIO: [
-        //   {
-        //     message: '请输入理论人次',
-        //     trigger: 'blur'
-        //   }
-        // ],
-        // CLASS_NUM: [
-        //   {
-        //     required: true,
-        //     message: '请选择类别',
-        //     trigger: 'blur'
-        //   }
-        // ],
-        // DEVICE_REMARK: [
-        //   {
-        //     message: '请选择仪器备注',
-        //     trigger: 'blur'
-        //   }
-        // ]
-      },
-      // 提交状态
-      loading: false,
-      // 是否是修改
-      isUpdate: false
-    };
-  },
-  computed: {
-    // 是否开启响应式布局
-    styleResponsive() {
-      return this.$store.state.theme.styleResponsive;
-    }
-  },
-  created() {},
-  methods: {
-    save() {
-      this.$refs.form.validate((valid) => {
-        if (!valid) {
-          return false;
-        }
-        // this.loading = true;
-        // console.log(this.data);
-        console.log(this.form);
-        this.form.YB_SP_MAN = this.$store.state.user.info.Nickname;
-        const loading = this.$messageLoading('请求中..');
-        ApproveYBSpdMainsjLinesIface(this.form)
-          .then((res) => {
-            this.$emit('done');
-            this.updateVisible(false);
-            this.$message.success(res.msg);
-          })
-          .catch((err) => {
-            this.$message.error(err.msg);
-          })
-          .finally(() => {
-            loading.close();
-          });
-      });
+  import {
+    ApproveYBSpdMainsjLinesIface,
+    BatchPushChargeItemToHis
+  } from '@/api/Home/masterBaseData';
+  export default {
+    components: {},
+    props: {
+      // 弹窗是否打开
+      visible: Boolean,
+      // 修改回显的数据
+      data: Array
     },
-    /* 更新visible */
-    updateVisible(value) {
-      this.$emit('update:visible', value);
-    }
-  },
-  watch: {
-    visible(visible) {
-      if (visible) {
-        // GetClassificProp_Jy().then((res) => {
-        //   // this.form.CLASS_NUM = res.result;
-        //   this.CLASS_NUM = res.result;
-        // });
-        if (this.data) {
-          // this.$util.assignObject(this.form, {
-          //   ...this.data
-          // });
-          var HEADER_IFACE_IDStr = '';
-          for (let i = 0; i < this.data.length; i++) {
-            HEADER_IFACE_IDStr += this.data[i].HEADER_IFACE_ID + ',';
+    data() {
+      const defaultForm = {
+        YB_SP_MARK: ''
+      };
+      return {
+        defaultForm,
+        // 表单数据
+        form: { ...defaultForm },
+        options: [],
+        // 表单验证规则
+        rules: {
+          // CONVERSION_RATIO: [
+          //   {
+          //     message: '请输入理论人次',
+          //     trigger: 'blur'
+          //   }
+          // ],
+          // CLASS_NUM: [
+          //   {
+          //     required: true,
+          //     message: '请选择类别',
+          //     trigger: 'blur'
+          //   }
+          // ],
+          // DEVICE_REMARK: [
+          //   {
+          //     message: '请选择仪器备注',
+          //     trigger: 'blur'
+          //   }
+          // ]
+        },
+        // 提交状态
+        loading: false,
+        // 是否是修改
+        isUpdate: false
+      };
+    },
+    computed: {
+      // 是否开启响应式布局
+      styleResponsive() {
+        return this.$store.state.theme.styleResponsive;
+      }
+    },
+    created() {},
+    methods: {
+      save() {
+        this.$refs.form.validate((valid) => {
+          if (!valid) {
+            return false;
           }
-          HEADER_IFACE_IDStr = HEADER_IFACE_IDStr.substring(
-            0,
-            HEADER_IFACE_IDStr.length - 1
-          );
-          this.form = {
-            ...this.defaultForm,
-            HEADER_IFACE_ID: HEADER_IFACE_IDStr
-          };
-          this.isUpdate = true;
+          this.loading = true;
+          this.form.YB_SP_MAN = this.$store.state.user.info.Nickname;
+          const loading = this.$messageLoading('请求中..');
+          this.handleApproveYBSpdMainsjLinesIface()
+            .then(() => {
+              this.handleBatchPushChargeItemToHis()
+                .then(() => {})
+                .finally(() => {
+                  this.$emit('done');
+                  loading.close();
+                });
+            })
+            .finally(() => {});
+        });
+      },
+
+      handleBatchPushChargeItemToHis() {
+        return new Promise((resolve, reject) => {
+          BatchPushChargeItemToHis({
+            HEADER_IFACE_ID: this.form.HEADER_IFACE_ID
+          })
+            .then((res) => {
+              resolve(res);
+              this.$message.success(res.msg);
+            })
+            .catch((err) => {
+              reject(err);
+              this.$message.error(err);
+            })
+            .finally(() => {});
+        });
+      },
+
+      handleApproveYBSpdMainsjLinesIface() {
+        return new Promise((resolve, reject) => {
+          ApproveYBSpdMainsjLinesIface(this.form)
+            .then((res) => {
+              resolve(res);
+              this.$message.success(res.msg);
+            })
+            .catch((err) => {
+              reject(err);
+              this.$message.error(err.msg);
+            })
+            .finally(() => {});
+        });
+      },
+
+      /* 更新visible */
+      updateVisible(value) {
+        this.$emit('update:visible', value);
+      }
+    },
+    watch: {
+      visible(visible) {
+        if (visible) {
+          // GetClassificProp_Jy().then((res) => {
+          //   // this.form.CLASS_NUM = res.result;
+          //   this.CLASS_NUM = res.result;
+          // });
+          if (this.data) {
+            // this.$util.assignObject(this.form, {
+            //   ...this.data
+            // });
+            var HEADER_IFACE_IDStr = '';
+            for (let i = 0; i < this.data.length; i++) {
+              HEADER_IFACE_IDStr += this.data[i].HEADER_IFACE_ID + ',';
+            }
+            HEADER_IFACE_IDStr = HEADER_IFACE_IDStr.substring(
+              0,
+              HEADER_IFACE_IDStr.length - 1
+            );
+            this.form = {
+              ...this.defaultForm,
+              HEADER_IFACE_ID: HEADER_IFACE_IDStr
+            };
+            this.isUpdate = true;
+          } else {
+            this.isUpdate = false;
+          }
         } else {
-          this.isUpdate = false;
+          this.$refs.form.clearValidate();
+          this.form = { ...this.defaultForm };
         }
-      } else {
-        this.$refs.form.clearValidate();
-        this.form = { ...this.defaultForm };
       }
     }
-  }
-};
+  };
 </script>
