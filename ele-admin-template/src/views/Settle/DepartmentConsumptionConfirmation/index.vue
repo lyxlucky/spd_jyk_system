7cffa39776ea3df662db2cde78dd044a35abdcb9
Jaime Peng <pengjianmim@gmail.com>
2026-04-10 16:18:16 +0800

    fix:一出扫码消耗

--- ele-admin-template/src/views/Settle/DepartmentConsumptionConfirmation/index.vue
+++ ele-admin-template/src/views/Settle/DepartmentConsumptionConfirmation/index.vue
@@ -104,6 +104,7 @@
           >
             <el-option label="申领出库" value="1" />
             <el-option label="扫码出库" value="2" />
+            <el-option label="一出扫码出库" value="3" />
           </el-select>
         </el-form-item>
         <el-form-item label="是否跟台">
