15835d047026265af01950dc9822430d578c14a4
zhb <test@jb.com>
2026-06-29 09:28:11 +0800

    fix：修复主页

--- ele-admin-template/src/router/routes.js
+++ ele-admin-template/src/router/routes.js
@@ -18,6 +18,11 @@ export const routes = [
     component: () => import('@/views/forget/index.vue'),
     meta: { title: '忘记密码' }
   },
+  {
+    path: '/KSInventory/MenuList',
+    component: () => import('@/views/KSInventory/MenuList/index.vue'),
+    meta: { title: '主页' }
+  },
   // 404
   {
     path: '*',
